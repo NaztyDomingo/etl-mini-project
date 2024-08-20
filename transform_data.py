559ba3b1e23dcaae340f86c84fd455839b3de8c6
Petter Saarela <petter.saarela@brightstraining.com>
2024-08-20 14:09:55 +0200

    updated for harmonization

--- transform_data.py
+++ transform_data.py
@@ -27,3 +27,48 @@ def met_to_dataframe(met_data):
         }
         rows.append(row)
     return pd.DataFrame(rows)
+
+# NEW STUFF TO TRY BELOW
+
+def transforming_smhi(smhi_df):
+
+    smhi_df['source'] = 'SMHI'
+
+    smhi_df['location'] = smhi_df['location'].replace({
+        '57.7_11.9': 'Gothenburg',
+        '55.6_13.0': 'Malmo',
+        '59.9_10.7': 'Oslo',
+        '59.3_18.0': 'Stockholm',
+        '60.1_24.9': 'Helsinki'
+        })
+    
+    smhi_df['cloud_coverage'] = (smhi_df['cloud_coverage'] / 8) * 100
+
+    smhi_df['time'] = pd.to_datetime(smhi_df['time'])
+    smhi_df['day_of_week'] = pd.to_datetime(smhi_df['time']).dt.day_name()
+    smhi_df['hour_of_day'] = pd.to_datetime(smhi_df['time']).dt.hour
+    smhi_df['time'] = smhi_df['time'].dt.strftime('%Y-%m-%d')
+    smhi_df = smhi_df.rename(columns={'time': 'date'})
+
+    return smhi_df
+
+def transforming_met(met_df):
+
+    met_df['source'] = 'MET'
+
+    met_df['location'] = met_df['location'].replace({
+        '57.7_11.9': 'Gothenburg',
+        '55.6_13.0': 'Malmo',
+        '59.9_10.7': 'Oslo',
+        '59.3_18.0': 'Stockholm',
+        '60.1_24.9': 'Helsinki'
+        })
+    
+    met_df['time'] = pd.to_datetime(met_df['time'])
+    met_df['day_of_week'] = pd.to_datetime(met_df['time']).dt.day_name()
+    met_df['hour_of_day'] = pd.to_datetime(met_df['time']).dt.hour
+    met_df['time'] = met_df['time'].dt.strftime('%Y-%m-%d')
+    met_df = met_df.rename(columns={'time': 'date'})
+
+
+    return met_df
